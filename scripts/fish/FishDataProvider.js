113b305f7dcbadcc4f95154b5826da7c0424c37f
Terra Roush <terra.roush@gmail.com>
2020-08-20 14:19:18 -0500

    created fishComponent and fishListComponent in their respective js modules, added imports and exports; used js to populate my html

---
+++ scripts/fish/FishDataProvider.js
@@ -0,0 +1,71 @@
+const fishCollection = [
+ 
+    {
+        image: "bartfish.gif",
+        imageAlt: "yellow Simpson's character Bart as a fish",
+        name: "Bart",
+        species: "Bartfish",
+        lengthInCM: 20,
+        harvestedFrom: "Springfield",
+        diet: "butterfinger"
+    },
+    {
+        image: "conlins angelfish.jpg",
+        imageAlt: "blue and yellow fish",
+        name: "Sam",
+        species: "Colin's Angelfish",
+        lengthInCM: 7.62,
+        harvestedFrom: "Philippines",
+        diet: "Spirulina, marine algae, mysis or frozen shrimp"
+    },
+    {
+        image: "gem tang.jpg",
+        imageAlt: "small black fish",
+        name: "Gandolf",
+        species: "Gem Tang",
+        lengthInCM: 22.86,
+        harvestedFrom: "Madagascar",
+        diet: "marine-based seaweed and algae"
+    },
+    {
+        image: "wrough iron butterfly fish.jpg",
+        imageAlt: "shiny black and white fish",
+        name: "Frodo",
+        species: "Wrought Iron Butterflyfish",
+        lengthInCM: 15.24,
+        harvestedFrom: "Southern Japan",
+        diet: "Microalgea, Zooplankton, Small crustaceans"
+    },
+    {
+        image: "pepp.jpg",
+        imageAlt: "red and white striped fish",
+        name: "Pippen",
+        species: "Peppermint Angelfish",
+        lengthInCM: 7,
+        harvestedFrom: "Rarotonga",
+        diet: "copepods and amphipods"
+    },
+    {
+        image: "pipefish-1.jpg",
+        imageAlt: "yellow striped long skinny fish",
+        name: "Gollum",
+        species: "Yellow Multi-Banded Pipefish",
+        lengthInCM: 16.5,
+        harvestedFrom: "Indonesia",
+        diet: "live copepods, vitamin-enriched live baby brine shrimp"
+    },
+    {
+        image: "fingered dragonet.jpg",
+        imageAlt: "dragon-like fish",
+        name: "Smaug",
+        species: "Dactylopus dactylopus",
+        lengthInCM: 30,
+        harvestedFrom: "Guam",
+        diet: "Zooplankton, small crustaceans, shrimp"
+    }
+    
+]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
