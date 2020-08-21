94f9d4af90f1d37f07460766622ba736f28e6723
Terra Roush <terra.roush@gmail.com>
2020-08-21 09:52:14 -0500

    created care component and a care list component; deleted zombie code/demonstrative code; mild reformat on index.html; set up js files for automating locations

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 import { useFish } from './fish/FishDataProvider.js';
 import { FishListComponent } from './fish/FishList.js';
-
-const allTheFish = useFish();
-console.log("all the fish", allTheFish);
-
-for (const fish of allTheFish) {
-    console.log("the item is", fish)
-}
+import { useCare } from './care/CareDataProvider.js'
+import { CareListComponent } from './care/CareListComponent.js'
 
 FishListComponent();
+
+// const allTheCare = useCare();
+
+// for (const care of allTheCare) {
+//     console.log(care)
+// }
+
+CareListComponent();
