b9966d1d7975d785e636f888394496c48075ae00
Terra Roush <terra.roush@gmail.com>
2020-08-23 09:31:51 -0500

    re-worded parameter in addFishToDom and inside for of loop for better understanding. also added commented description of what different functions do

--- scripts/fish/FishListComponent.js
+++ scripts/fish/FishListComponent.js
@@ -1,27 +1,28 @@
 import { useFishObjArr, makeMostHolyFish, makeSoldierFish, makeUnworthyFish } from './FishDataProvider.js';
 import { FishComponent } from './fishComponent.js';
 
-// this function: defines a variable that accesses the place in the dom where you want to put your fish components; calls a function that gives us an array that holds a  copy of our fish objects; creates an empty string for our trasformed objects; iterates over the location objects, and adds the fish objects, one by one to the empty string; finally with dot notation and innerHTML, we transform the objects into strings with template literals, to be displayed on the dom. 
+// This function combines with addFishToDom. This one defines variables that refer to the objects in an array so that we can transform the object data into strig data to put into the dom.
 
 export const FishListComponent = () => {
     // const fishes = useFishObjArr();
-    const holiest = makeMostHolyFish();
-    addFishToDom(holiest);
-    const soldiers = makeSoldierFish();
-    addFishToDom(soldiers);
-    const unworthy = makeUnworthyFish();
-    addFishToDom(unworthy);
+    const holiestArr = makeMostHolyFish();
+    addFishToDom(holiestArr);
+    const soldiersArr = makeSoldierFish();
+    addFishToDom(soldiersArr);
+    const unworthyArr = makeUnworthyFish();
+    addFishToDom(unworthyArr);
 }   
- 
-const addFishToDom = (whichFishArray) => {
 
+// this function accepts an array. aFishArr acts a a placeholder for whatever array will be used. inside this function: it defines a variable that accesses the place in the dom where you want to put your fish components; iterates over the fish objects from a fish array, and adds the fish objects, one by one to the empty string; finally with dot notation and innerHTML, we transform the objects into strings with template literals, to be displayed on the dom. 
+
+const addFishToDom = (aFishArr) => {
     const contentElement = document.querySelector(".fishList");
 
     let fishHTMLRepresentation = "";
-    for (const oneThingFromTheSea of whichFishArray) {
-        fishHTMLRepresentation += FishComponent(oneThingFromTheSea);
+    for (const fishObj of aFishArr) {
+        fishHTMLRepresentation += FishComponent(fishObj);
     }
-     // Add to the existing HTML in the content element
+     // Add to the existing HTML by using dot natation on contentElement
     contentElement.innerHTML += `
         ${fishHTMLRepresentation}
     `
